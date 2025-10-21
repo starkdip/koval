c630ff3c8ec74bb5f534b00c4a8f5d3db752c589
starkdip <kovald535@gmail.com>
2025-10-21 21:36:21 +0700

    Добавлен класс Car

---
+++ Car.java
@@ -0,0 +1,34 @@
+public class Car {
+    private int id;
+    private String brand;
+    private String model;
+    private int year;
+    private String color;
+    private double price;
+    private String registrationNumber;
+
+    public Car(int id,String brand,String mode, int year, String color, double price, String registrationNumber){
+        this.id=id;
+        this.brand=brand;
+        this.model=model;
+        this.year=year;
+        this.color=color;
+        this.price=price;
+        this.registrationNumber=registrationNumber;
+    }
+    public int getId(){return id;}
+    public void setId(int id) {this.id=id;}
+    public String getBrand(){return brand;}
+    public void setBrand(String brand) {this.brand=brand;}
+    public String getModel(){return brand;}
+    public void setModel(String model) {this.model=model;}
+    public int getYear(){return year;}
+    public void setYear(String year) {this.year=year;}
+    public String getColor(){return color;}
+    public void setColor(String color) {this.color=color;}
+    public double getPrice(){return price;}
+    public void setPrice(double price) {this.price=price;}
+    public String getRegistrationNumber(){return registrationNumber;}
+    public void setRegistrationNumber(String registrationNumber) {this.registrationNumber=registrationNumber;}
+    
+}
