3964c81bdec908c500ac6a24b019215a38f4155f
starkdip <kovald535@gmail.com>
2025-11-05 23:32:36 +0700

    T getitemByIndex(int i)

--- 8.java
+++ 8.java
@@ -37,4 +37,10 @@ public class Cache<T>{
         }
         return items.getLast();
     }
+    public T getItemByIndex(int index){
+        if (index<0||index>=items.size()){
+            return null;
+        }
+        return items.get(index);
+    }
 }
