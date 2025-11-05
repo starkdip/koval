4bccee5cba55ba85aca2d2323f336f81be87c1ac
starkdip <kovald535@gmail.com>
2025-11-06 00:00:20 +0700

    bool exists(T item)

--- 8.java
+++ 8.java
@@ -21,4 +21,7 @@ public class Cache<T>{
     public boolean remove(T item) {
         return items.remove(item);
     }
+    public boolean exists(T item) {
+        return items.contains(item);
+    }
 }
